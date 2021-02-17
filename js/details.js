071e58c881b9bdd555d61bfaf84a3bf93518b595
NukLaochaem <71333106+NukLaochaem@users.noreply.github.com>
2021-02-17 10:21:03 +0100

    id api

---
+++ js/details.js
@@ -0,0 +1,43 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+console.log(params);
+
+const pokeUrl = "https://api.pokemontcg.io/v2/cards?q=name:gardevoir" + id;
+const proxy = "https://noroffcors.herokuapp.com/";
+
+const containerDetails = document.querySelector(".container_details")
+const details = document.querySelector(".details");
+
+containerDetails.innerHTML = id;
+
+async function callApi(){
+    try{
+        const response = await fetch(pokeUrl);
+        const json = await response.json(response);
+        const results = json.data;
+        
+        console.log(results);
+
+        for(let i = 1; i < results.length; i++){
+            
+
+            console.log(results[i]);
+
+            containerDetails.innerHTML += `<h3>${results[i].name}</h3>
+                                                <img src="${results[i].images.small}"></img>
+            `;
+        }
+    } catch (error) {
+        console.log("didnt work");
+        detail.innerHTML ="Error, Cannot load the page"
+    }
+}
+
+callApi();
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,72 @@
+
+const select = document.querySelector("select")
+const memeContainer = document.querySelector(".memeContainer")
+
+
+const url = "https://api.pokemontcg.io/v2/cards?q=name:gardevoir";
+const proxy = "https://noroffcors.herokuapp.com/";
+
+
+
+async function api(){
+    try{
+        const response = await fetch(url);
+        const json = await response.json(response);
+        const pokemon = json.data
+
+        console.log();
+
+        for(let i = 0; i < pokemon.length; i++){
+            if(i === 6){
+                break;
+            }
+            console.log(pokemon[i]);
+
+            memeContainer.innerHTML += `<a href="details.html?id=${pokemon[i].id}">  
+                                        <img src="${pokemon[i].images.small}"></img>
+                                        </a>`;
+        }                               
+
+        
+    } catch (error) {
+        console.log(" pic didnt work");
+        memeContainer.innerHTML ="Error, Cannot load the page"
+    }
+}
+api()
+
+
+
+
+
+
+
+
+
+const jokecontainer = document.querySelector(".jokes");
+
+const jokesUrl = "https://official-joke-api.appspot.com/jokes/ten";
+
+
+async function jokeAPI(){
+    try{
+        const respon = await fetch(jokesUrl);
+        const getJson = await respon.json(respon);
+
+        //console.log(getJson);
+
+        for(let i = 0; i < getJson.length; i++){ 
+            
+        jokecontainer.innerHTML += `<div class="joke_container">
+                                        <div> ${getJson[i].setup} </div>
+                                        <div> Answer: ${getJson[i].punchline} </div>
+                                    </div>`;
+        }
+    } catch (error){
+        console.log("not working fix me");
+    }
+}
+jokeAPI();
+
+
+
